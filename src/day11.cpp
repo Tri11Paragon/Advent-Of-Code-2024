90bf6ae125532040182b2914983514720242a0fa
Brett <brettmaster1@gmail.com>
2024-12-12 11:41:46 -0500

    bad solution

--- src/day11.cpp
+++ src/day11.cpp
@@ -28,6 +28,77 @@
 #include <blt/math/vectors.h>
 #include <blt/std/assert.h>
 
+void apply_rules(std::vector<blt::size_t>& stones)
+{
+    for (auto it = stones.begin(); it != stones.end(); ++it)
+    {
+        auto& val = *it;
+
+        if (val == 0)
+        {
+            val = 1;
+        } else if (auto str = std::to_string(val); str.size() % 2 == 0)
+        {
+            auto last = str.substr(str.size() / 2);
+            auto first = str.substr(0, str.size() / 2);
+            auto pos = stones.insert(stones.erase(it), std::stoull(last));
+            auto pos2 = stones.insert(pos, std::stoull(first));
+            it = ++pos2;
+        } else
+        {
+            val *= 2024;
+        }
+    }
+}
+
+blt::hashmap_t<blt::vec2ul, blt::size_t> caches;
+
+blt::size_t fuck_dynamic_programming(blt::size_t stone, blt::size_t depth)
+{
+    if (caches.contains(blt::vec2ul{stone, depth}))
+        return caches[blt::vec2ul{stone, depth}];
+    if (depth == 0)
+        return 1;
+    blt::size_t result = 0;
+    if (stone == 0)
+        result = fuck_dynamic_programming(1, depth - 1);
+    else if (const auto str = std::to_string(stone); str.size() % 2 == 0)
+    {
+        result = fuck_dynamic_programming(std::stoull(str.substr(0, str.size() / 2)), depth - 1);
+        result += fuck_dynamic_programming(std::stoull(str.substr(str.size() / 2)), depth - 1);
+    } else
+        result = fuck_dynamic_programming(stone * 2048, depth - 1);
+    caches[blt::vec2ul{stone, depth}] = result;
+    return result;
+}
+
+void print(const std::vector<blt::size_t>& stones)
+{
+    for (const auto i : stones)
+        std::cout << i << " ";
+    std::cout << std::endl;
+}
+
 void run_day11()
 {
+    auto file = blt::fs::getFile("../problems/day11/input");
+
+    std::vector<blt::size_t> stones;
+
+    auto stone_values = blt::string::split(file, ' ');
+    for (const auto& v : stone_values)
+        stones.push_back(std::stoull(v));
+
+    auto part2_copy = stones;
+
+    for (int i = 0; i < 25; i++)
+        apply_rules(stones);
+
+    BLT_TRACE(stones.size());
+
+    blt::size_t total = 0;
+    for (const auto i : part2_copy)
+        total += fuck_dynamic_programming(i, 75);
+    BLT_TRACE(total);
+
 }
--- src/day12.cpp
+++ src/day12.cpp
@@ -20,6 +20,7 @@
 #include <vector>
 #include <limits>
 #include <algorithm>
+#include <queue>
 #include <blt/fs/loader.h>
 #include <blt/iterator/enumerate.h>
 #include <blt/iterator/iterator.h>
@@ -28,6 +29,182 @@
 #include <blt/math/vectors.h>
 #include <blt/std/assert.h>
 
+namespace day12
+{
+    std::vector<std::vector<char>> plots;
+    std::vector<std::vector<int>> perimeters;
+    std::vector<std::vector<int>> params;
+    std::vector<blt::vec2i> founds;
+    blt::hashset_t<blt::vec2i> visited;
+    blt::hashset_t<blt::vec2i> visited_side;
+
+    int width, height;
+
+    template <typename T>
+    std::optional<blt::ref<std::remove_reference_t<decltype(std::declval<T>()[0][0])>>> get(T& t, const int x, const int y)
+    {
+        if (x < 0 || y < 0 || x >= width || y >= height)
+            return {};
+        return t[y][x];
+    }
+
+    blt::vec2i floodfill(const char prev, const int x, const int y)
+    {
+        if (visited.contains({x, y}) || x < 0 || y < 0 || x >= width || y >= height)
+            return {0, 0};
+        const auto cur = plots[y][x];
+        if (cur != prev)
+            return {0, 0};
+        visited.insert({x, y});
+        return blt::vec2i{1, perimeters[y][x]} + floodfill(cur, x + 1, y) + floodfill(cur, x - 1, y) + floodfill(cur, x, y + 1) +
+            floodfill(cur, x, y - 1);
+    }
+
+    blt::vec2i flood(const int x, const int y)
+    {
+        return floodfill(plots[y][x], x, y);
+    }
+
+    template <typename T>
+    void print(const T& t)
+    {
+        for (int j = 0; j < height; j++)
+        {
+            for (int i = 0; i < width; i++)
+                std::cout << t[j][i] << " ";
+            std::cout << std::endl;
+        }
+    }
+}
+
 void run_day12()
 {
+    using namespace day12;
+    auto file = blt::fs::getLinesFromFile("../problems/day12/example1");
+
+    width = static_cast<int>(file.begin()->size());
+    height = static_cast<int>(file.size());
+
+    for (const auto& line : file)
+    {
+        plots.emplace_back();
+        perimeters.emplace_back();
+        for (const auto c : line)
+        {
+            plots.back().push_back(c);
+            perimeters.back().push_back(-1);
+        }
+    }
+
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            int per = 0;
+            const auto cur = plots[y][x];
+            if (const auto v = get(plots, x + 1, y); !v || v->get() != cur)
+                per++;
+            if (const auto v = get(plots, x - 1, y); !v || v->get() != cur)
+                per++;
+            if (const auto v = get(plots, x, y + 1); !v || v->get() != cur)
+                per++;
+            if (const auto v = get(plots, x, y - 1); !v || v->get() != cur)
+                per++;
+            perimeters[y][x] = per;
+        }
+    }
+
+    int total = 0;
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            auto fill = flood(x, y);
+            const auto size = fill.x() * fill.y();
+            total += size;
+        }
+    }
+
+    BLT_TRACE(total);
+
+    int total2 = 0;
+    visited.clear();
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            if (visited_side.contains({x, y}))
+                continue;
+            auto cur = plots[y][x];
+            int edges = 0;
+            int area = flood(x, y).x();
+
+            std::queue<blt::vec2i> pos_to_check;
+            pos_to_check.push({x, y});
+
+            while (!pos_to_check.empty())
+            {
+                auto pos = pos_to_check.front();
+                pos_to_check.pop();
+
+                if (visited_side.contains(pos))
+                    continue;
+
+                bool x_pos = false, x_neg = false, y_pos = false, y_neg = false;
+
+                {
+                    const auto v = get(plots, pos.x() + 1, pos.y());
+                    x_pos = v && v->get() == cur;
+                }
+                {
+                    const auto v = get(plots, pos.x() - 1, pos.y());
+                    x_neg = v && v->get() == cur;
+                }
+                {
+                    const auto v = get(plots, pos.x(), pos.y() + 1);
+                    y_pos = v && v->get() == cur;
+                }
+                {
+                    const auto v = get(plots, pos.x(), pos.y() - 1);
+                    y_neg = v && v->get() == cur;
+                }
+
+                if (x_pos)
+                    pos_to_check.push({pos.x() + 1, pos.y()});
+                if (y_pos)
+                    pos_to_check.push({pos.x(), pos.y() + 1});
+                if (x_neg)
+                    pos_to_check.push({pos.x() - 1, pos.y()});
+                if (y_neg)
+                    pos_to_check.push({pos.x(), pos.y() - 1});
+
+                auto b4 = edges;
+                if ((x_pos && (y_pos && !y_neg) && !x_neg) || (x_pos && (!y_pos && y_neg) && !x_neg))
+                    edges++;
+                else if ((y_neg && (x_pos && !x_neg) && !y_pos) || (y_neg && (!x_pos && x_neg) && !y_pos))
+                    edges++;
+                else if ((x_neg && (y_pos && !y_neg) && !x_pos) || (x_neg && (!y_pos && y_neg) && !x_pos))
+                    edges++;
+                else if ((y_pos && (x_pos && !x_neg) && !y_neg) || (y_pos && (!x_pos && x_neg) && !y_neg))
+                    edges++;
+
+
+                if (x_neg && !x_pos && !y_pos && !y_neg || !x_neg && x_pos && !y_pos && !y_neg)
+                    edges+=2;
+                if (!x_neg && !x_pos && y_pos && !y_neg || !x_neg && !x_pos && !y_pos && y_neg)
+                    edges+=2;
+                if (!x_neg && !x_pos && !y_pos && !y_neg)
+                    edges += 4;
+
+                BLT_TRACE("Visit %c at %d %d edges %d (%d)", cur, pos.x(), pos.y(), edges, edges-b4);
+
+
+                visited_side.insert(pos);
+
+            }
+            BLT_TRACE("For %c do %d edges with %d area", cur, edges, area);
+            total2 += edges * area;
+        }
+    }
+    BLT_TRACE(total2);
 }
